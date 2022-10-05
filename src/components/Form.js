74661c7f8ff906f4d8597ea95e583710bacf7448
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-10-05 14:41:31 +0300

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,48 +1,80 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 function Form() {
+const [formData, setFormData] =useState({})
+
+const redirect = useNavigate()
+  function handleInput(e) {
+    const name = e.target.name
+    const value = e.target.value
+    setFormData({...formData,[name]:value}) 
+  }
+  function handleSubmit() {
+
+    fetch("http://localhost:3000/records",{
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    redirect("/")
+    })
+  }
+
+  //console.log(formData)
+  // "id": "2",
+  //         "date": "2/2/2022",
+  //         "pair": "EUR/USD",
+  //         "open_price": "96.45",
+  //         "closed_price": "112.23",
+  //         "image_url": "",
+  //         "profit": "-98
+ 
   return (
     <div className="flex flex-col items-center rounded  space-y-10 bg-slate-300 p-6 my-4 my-auto">
         <div className="col-span-6 leading-8">
         <label>
         Date:
-        <input type="date" />
+        <input onChange={handleInput} type="date" name="date" />
       </label>
         </div>
      
      <div>
      <label>
         Pair:
-        <input type="text" />
+        <input onChange={handleInput} type="text" name="pair" />
       </label>
      </div>
      <div>
      <label>
         Open Price:
-        <input type="number" />
+        <input onChange={handleInput} type="number" name="open_price" />
       </label>
      </div>
      <div>
      <label>
         Close Price:
-        <input type="text" />
+        <input onChange={handleInput} type="text" name="closed_price" />
       </label>
      </div>
      <div>
      <label>
         Img url:
-        <input type="text" />
+        <input onChange={handleInput} type="text" name="image_url" />
       </label>
      </div>
      <div>
      <label>
         Price:
-        <input type="number" />
+        <input onChange={handleInput} type="number" name="profit" />
       </label>
-      <button type="button" className="bg-indigo-500 ..." disabled>
-  <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
-    
-  </svg>
-  Processing...
-</button>
+     <button type="submit" onClick={handleSubmit} className="bg-sky-300">submit</button>
      </div>
     </div>
   );
